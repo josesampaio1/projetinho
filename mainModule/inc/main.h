a4ed58f104fdda6d06c08029127ffa6c09e8a5a9
Jose Sampaio <jsampaioifce@gmail.com>
2023-04-16 21:44:35 +0200

    initial commit

---
+++ mainModule/inc/main.h
@@ -0,0 +1,12 @@
+#include <print_util.h>
+#include <stdio.h>
+#include "../../stringModule/inc/string_util.h"
+#include "../../printModule/inc/print_util.h"
+
+#ifndef MAIN
+#define MAIN
+
+char * input_source ();
+char * input_target ();
+
+#endif 
